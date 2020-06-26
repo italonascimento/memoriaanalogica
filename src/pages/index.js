9b66c2dd90fe2b3d964801242e1d362e67cebefd
Ítalo Nascimento <italo.nascimento@ammovarejo.com.br>
2020-06-26 18:05:33 -0300

    implement basic square payment functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,28 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import PaymentForm from '../components/PaymentForm'
+import { Helmet } from "react-helmet"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {  
+  return (
+    <Layout>
+      <Helmet>
+        <meta httpEquiv="Content-Security-Policy" content="script-src 'self' 'unsafe-inline' *.squarecdn.com *.google.com" />
+      </Helmet>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
+      <PaymentForm paymentForm={window.SqPaymentForm} amount={1} />
+    </Layout>
+  )
+}
 
 export default IndexPage
