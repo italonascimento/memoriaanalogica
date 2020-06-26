f69991ed9c08b109d7f101bddc0a850290d4a8b7
Ítalo Nascimento <italo.nascimento@ammovarejo.com.br>
2020-06-26 18:10:21 -0300

    check if window is defined

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,9 @@ const IndexPage = () => {
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
 
-      <PaymentForm paymentForm={window.SqPaymentForm} amount={1} />
+      {window && (
+        <PaymentForm paymentForm={window.SqPaymentForm} amount={1} />
+      )}
     </Layout>
   )
 }
