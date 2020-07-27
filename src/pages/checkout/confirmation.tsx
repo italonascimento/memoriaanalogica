45cb43e55017496990da08a1df361e99bec03bf6
Ítalo Nascimento <italo.nascimento@ammovarejo.com.br>
2020-07-27 02:24:43 -0300

    implement basic confirmation page

--- src/pages/checkout/confirmation.tsx
+++ src/pages/checkout/confirmation.tsx
@@ -7,13 +7,20 @@ import SEO from '../../components/seo'
 import useTranslation from '../../components/hooks/useTanslation'
 import useGlobalState from '../../state/useGlobalState'
 import { actions } from '../../state/cart-state'
+import styled, { ThemeProps } from 'styled-components'
+import Spacing from '../../components/atoms/spacing'
+import Button from '../../components/atoms/button'
+import { navigate } from 'gatsby-plugin-intl'
+import { IoIosCheckboxOutline } from 'react-icons/io'
+import { Theme } from '../../themes/default-theme'
+import mediaQueries from '../../styles/media-queries'
 
 interface ConfirmationProps {
   location: WindowLocation<{ payment: PaymentResponse }>
 }
 
 const Confirmation = ({ location }: ConfirmationProps) => {
-  const t = useTranslation('confirmation')
+  const t = useTranslation('checkout.confirmation')
   const [_, dispatch] = useGlobalState(s => s)
   const payment = location.state?.payment
 
@@ -24,11 +31,51 @@ const Confirmation = ({ location }: ConfirmationProps) => {
   return (
     <Layout>
       <SEO title={t('title')} />
-      <div>
-        {JSON.stringify(payment)}
-      </div>
+      <Container>
+        <IoIosCheckboxOutline size={128} />
+        <Spacing y={32} />
+        <Title>{t('thank_you')}</Title>
+        <Spacing y={16} />
+        <Text>{t('follow_progress_by_email')}</Text>
+        <Spacing y={32} />
+        <Button large primary onClick={() => navigate('/')}>
+          {t('back_to_home')}
+        </Button>
+      </Container>
     </Layout>
   )
 }
+
+const Container = styled.div`
+  padding: 0 16px;
+  max-width: 620px;
+  height: 100%;
+  margin: 0 auto;
+  color: ${(props: ThemeProps<Theme>) => props.theme.colors.greyDark2};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & > * {
+    flex-shrink: 0;
+  }
+`
+
+const Title = styled.h2`
+  font-size: 40px;
+
+  ${mediaQueries.md} {
+    font-size: 64px;
+  }
+`
+
+const Text = styled.p`
+  font-size: 18px;
+
+${mediaQueries.md} {
+  font-size: 24px;
+}
+`
 
 export default Confirmation
